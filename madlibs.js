8385bffa6aae69821cfb33f47b983321bba29722
Bree Hall <briannajdhall@gmail.com>
2021-10-31 11:23:52 -0400

    Revised some of the mobile styling for the completed story and added a Reset Madlibs button to restart the game

--- madlibs.js
+++ madlibs.js
@@ -9,13 +9,14 @@ var userSubmission = {};
 const submitMadLibs = (event) => {
     //required to prevent the form from reloading on submit
     event.preventDefault();
-    madLibsForm.style.display = "none";
+    madLibsForm.classList.add('hide');
 
     const form = new FormData(event.target);
     userSubmission = Object.fromEntries(form);
     console.log(userSubmission.number);
 
     let story = `
+        <h3>Your completed story:</h3>
         <p>Winter is one of <span class="inserted-text">${userSubmission.number}</span> seasons of the year. 
         The other seasons are <span class="inserted-text">${userSubmission.adjective_1}</span>, <span class="inserted-text">${userSubmission.noun_1}</span>, and
         <span class="inserted-text"> ${userSubmission.noun_2}</span>.
@@ -23,17 +24,23 @@ const submitMadLibs = (event) => {
         is the furthest from the Earth. The weather tends to be <span class="inserted-text">${userSubmission.adjective_2}</span> in the winter, with
         <span class="inserted-text">${userSubmission.noun_4} </span>
         fall and cold temperatures. Some winter sports include <span class="inserted-text">${userSubmission.sport_1}</span>, 
-        <span class="inserted-text">${userSubmission.sport_2}</span>, 
-        and <span class="inserted-text">${userSubmission.sport_3}</span>. Hot <span class="inserted-text">${userSubmission.beverage}</span>
+        <span class="inserted-text">${userSubmission.sport_2}</span>, and <span class="inserted-text">${userSubmission.sport_3}</span>.
+        Hot <span class="inserted-text">${userSubmission.beverage}</span>
          with <span class="inserted-text">${userSubmission.food}</span> on top is a popular winter drink.</p>
     `;
 
-    storySection.innerHTML = story;
+    storySection.innerHTML += story;
     storySection.classList.remove('hide');
 
+    let resetButton = `
+        <button id="madlibs-reset" onclick="resetMadLibs()">Play Again</button>
+    `;
+    storySection.innerHTML += resetButton;
 }
 
-const loadStory = (userSubmission) => {
-   
+const resetMadLibs = () => {
+    storySection.classList.add('hide');
+    storySection.innerHTML = '';
+    madLibsForm.reset();
+    madLibsForm.classList.remove('hide');
 }
-
